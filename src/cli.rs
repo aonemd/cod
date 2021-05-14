e37f9315d97ff66ba1569659411e0e4f27b7b3ce
aonemd <aonemdsaleh@gmail.com>
2021-05-14 12:44:22 +0200

    Add subcommand descriptions

--- src/cli.rs
+++ src/cli.rs
@@ -13,20 +13,29 @@ pub struct Cli {
 #[derive(Debug, StructOpt)]
 pub enum Command {
     #[structopt(name = "list", visible_alias = "l")]
+    /// List all items
     List,
+
+    /// Add an item. Date starts with a `@`. Tags start with a `+`
     #[structopt(name = "add", visible_alias = "a")]
     Add {
         content: Vec<String>,
     },
+
+    /// Edit an item. Attributes not passed will not be changed
     #[structopt(name = "edit", visible_alias = "e")]
     Edit {
         id: u32,
         content: Vec<String>,
     },
+
+    /// Toggle completed status of one or more items using their ids
     #[structopt(name = "complete", visible_alias = "c")]
     Complete {
         ids: Vec<u32>,
     },
+
+    /// Delete one or more items using their ids
     #[structopt(name = "delete", visible_alias = "d")]
     Delete {
         ids: Vec<u32>,
